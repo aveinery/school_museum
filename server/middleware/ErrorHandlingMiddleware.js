75163131af80875c36d056be485f0f10bf47deac
Anastasia <aveinery@yandex.ru>
2024-06-09 17:37:29 +0300

    fix

--- server/middleware/ErrorHandlingMiddleware.js
+++ server/middleware/ErrorHandlingMiddleware.js
@@ -1,10 +1,10 @@
-import ApiError from "../error/apiError.js"
+import ApiError from '../error/apiError.js';
 
 const errorHandlingMiddleware = function (err, req, res, next) {
   if (err instanceof ApiError) {
-    return res.status(err.status).json({ message: err.message })
+    return res.status(err.status).json({ message: err.message });
   }
-  return res.status(500).json({ message: "Непредвиденная ошибка!" })
-}
+  return res.status(500).json({ message: 'Непредвиденная ошибка!' });
+};
 
-export default errorHandlingMiddleware
+export default errorHandlingMiddleware;
